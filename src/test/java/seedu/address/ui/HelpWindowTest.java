c8872746004d935a85bf1cd1c5155ee341cebedc
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2024-03-30 17:46:58 +0800

    Fix import order

--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -3,9 +3,9 @@ package seedu.address.ui;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
