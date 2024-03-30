ca907f4a6fed47044334693f9258de2bc55d6004
Hibeom0929 <E1093045@u.nus.edu>
2024-03-31 00:40:05 +0800

    Adjust minor error

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -15,10 +15,7 @@ import seedu.address.model.person.NameContainsKeywordsPredicate;
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names or course contain "
-            + "any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-
+    
     public static final String COMMAND_DESCRIPTION = COMMAND_WORD + ": Finds all persons whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n";
 
