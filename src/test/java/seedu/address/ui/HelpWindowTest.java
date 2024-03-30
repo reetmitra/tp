321c664e357ba832b3544de752d24495f168abb9
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2024-03-30 18:01:53 +0800

    Update HelpWindowTest.java

--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -11,7 +11,6 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-import javafx.application.Platform;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
@@ -29,35 +28,18 @@ public class HelpWindowTest {
     @DisabledOnOs(OS.LINUX)
     @Test
     public void closeOnEscapeKeyPress(FxRobot robot) {
-        Platform.runLater(() -> {
-            helpWindow.show();
-            robot.press(KeyCode.ESCAPE);
-        });
-
-        try {
-            Thread.sleep(2000);
-            assertFalse(helpWindow.isShowing());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        assertTrue(helpWindow.isShowing());
+        robot.press(KeyCode.ESCAPE);
+        assertFalse(helpWindow.isShowing());
     }
 
     @Test
     public void remainOpenOnNonEscapeKeyPress(FxRobot robot) {
-        Platform.runLater(() -> {
-            helpWindow.show();
-            robot.press(KeyCode.A);
-            robot.press(KeyCode.SOFTKEY_0);
-            robot.press(KeyCode.L);
-            robot.press(KeyCode.BACK_SPACE);
-            robot.press(KeyCode.DELETE);
-        });
-
-        try {
-            Thread.sleep(2000);
-            assertTrue(helpWindow.isShowing());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        robot.press(KeyCode.A);
+        robot.press(KeyCode.SOFTKEY_0);
+        robot.press(KeyCode.L);
+        robot.press(KeyCode.BACK_SPACE);
+        robot.press(KeyCode.DELETE);
+        assertTrue(helpWindow.isShowing());
     }
 }
