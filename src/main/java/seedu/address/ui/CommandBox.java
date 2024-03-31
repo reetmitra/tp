67d3761fec5faa1d6132046fc8dc5257c14bb3ea
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2024-03-31 23:13:31 +0800

    Add UP and DOWN keys to scroll past commands

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,8 +1,13 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -17,6 +22,9 @@ public class CommandBox extends UiPart<Region> {
     private static final String FXML = "CommandBox.fxml";
 
     private final CommandExecutor commandExecutor;
+    private final List<String> commandHistory;
+
+    private int commandHistoryIndex;
 
     @FXML
     private TextField commandTextField;
@@ -27,6 +35,9 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(CommandExecutor commandExecutor) {
         super(FXML);
         this.commandExecutor = commandExecutor;
+        this.commandHistory = new ArrayList<>();
+        this.commandHistoryIndex = 0;
+
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
     }
@@ -42,10 +53,29 @@ public class CommandBox extends UiPart<Region> {
         }
 
         try {
+            commandHistory.add(commandText);
+            commandHistoryIndex = commandHistory.size(); // Reset index.
+
             commandExecutor.execute(commandText);
             commandTextField.setText("");
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
+        }
+    }
+
+    /**
+     * Handles any key button pressed event. Currently supports only UP and DOWN keys.
+     *
+     * @param keyEvent The key pressed while command box is in focus.
+     */
+    @FXML
+    private void handleKeyPressed(KeyEvent keyEvent) {
+        final KeyCode keyCode = keyEvent.getCode();
+
+        if (keyCode == KeyCode.UP) {
+            commandTextField.setText(getPreviousCommandHistory(commandTextField.getText()));
+        } else if (keyCode == KeyCode.DOWN) {
+            commandTextField.setText(getNextCommandHistory(commandTextField.getText()));
         }
     }
 
@@ -67,6 +97,35 @@ public class CommandBox extends UiPart<Region> {
         }
 
         styleClass.add(ERROR_STYLE_CLASS);
+    }
+
+    /**
+     * Get the next command in the command history. If the cursor is already at the last command,
+     * then an empty string will be returned. If the command history is empty, then the given
+     * {@code defaultText} will be returned instead.
+     */
+    private String getNextCommandHistory(String defaultText) {
+        if (commandHistory.isEmpty()) {
+            return defaultText;
+        } else {
+            commandHistoryIndex = Math.min(commandHistoryIndex + 1, commandHistory.size());
+            return (commandHistoryIndex == commandHistory.size())
+                ? ""
+                : commandHistory.get(commandHistoryIndex);
+        }
+    }
+
+    /**
+     * Get the previous command in the command history. If the command history is empty, then the given
+     * {@code defaultText} will be returned instead.
+     */
+    private String getPreviousCommandHistory(String defaultText) {
+        if (commandHistory.isEmpty()) {
+            return defaultText;
+        } else {
+            commandHistoryIndex = Math.max(commandHistoryIndex - 1, 0);
+            return commandHistory.get(commandHistoryIndex);
+        }
     }
 
     /**
