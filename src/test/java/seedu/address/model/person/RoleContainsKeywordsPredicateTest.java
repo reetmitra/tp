7a266d6875b527cd4418934c047644494c187dc9
Yang Heebeom <e1093045@u.nus.edu>
2024-04-04 00:35:51 +0800

    Enable find people by role

---
+++ src/test/java/seedu/address/model/person/RoleContainsKeywordsPredicateTest.java
@@ -0,0 +1,84 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
+
+public class RoleContainsKeywordsPredicateTest {
+
+    @Test
+    public void equals() {
+        List<String> firstPredicateKeywordList = Collections.singletonList("first");
+        List<String> secondPredicateKeywordList = Arrays.asList("first", "second");
+
+        RoleContainsKeywordsPredicate firstPredicate = new RoleContainsKeywordsPredicate(firstPredicateKeywordList);
+        RoleContainsKeywordsPredicate secondPredicate =
+                new RoleContainsKeywordsPredicate(secondPredicateKeywordList);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        RoleContainsKeywordsPredicate firstPredicateCopy =
+                new RoleContainsKeywordsPredicate(firstPredicateKeywordList);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void test_RoleContainsKeywords_returnsTrue() {
+        // One keyword
+        RoleContainsKeywordsPredicate predicate =
+                new RoleContainsKeywordsPredicate(Collections.singletonList("TA"));
+        assertTrue(predicate.test(new PersonBuilder().withRole("TA").build()));
+
+        // Only one matching keyword
+        predicate = new RoleContainsKeywordsPredicate(Arrays.asList("PROFESSOR", "TA"));
+        assertTrue(predicate.test(new PersonBuilder().withRole("TA").build()));
+
+        // Mixed-case keywords
+        predicate = new RoleContainsKeywordsPredicate(Arrays.asList("student"));
+        assertTrue(predicate.test(new PersonBuilder().withRole("STUDENT").build()));
+    }
+
+    @Test
+    public void test_nameDoesNotContainKeywords_returnsFalse() {
+        // Zero keywords
+        RoleContainsKeywordsPredicate predicate = new RoleContainsKeywordsPredicate(Collections.emptyList());
+        assertFalse(predicate.test(new PersonBuilder().withRole("STUDENT").build()));
+
+        // Non-matching keyword
+        predicate = new RoleContainsKeywordsPredicate(Arrays.asList("student"));
+        assertFalse(predicate.test(new PersonBuilder().withRole("TA").build()));
+
+        // Keywords match phone, email and address, but does not match role
+        predicate = new RoleContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
+        assertFalse(predicate.test(new PersonBuilder().withRole("TA").withPhone("12345")
+                .withEmail("alice@email.com").withAddress("Main Street").build()));
+    }
+
+    @Test
+    public void toStringMethod() {
+        List<String> keywords = List.of("keyword1", "keyword2");
+        RoleContainsKeywordsPredicate predicate = new RoleContainsKeywordsPredicate(keywords);
+
+        String expected = RoleContainsKeywordsPredicate.class.getCanonicalName() + "{keywords=" + keywords + "}";
+        assertEquals(expected, predicate.toString());
+    }
+}
